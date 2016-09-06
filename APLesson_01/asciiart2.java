f01e63c68d16956ee1a3222c2b825cb79dd3d090
laurenzhangg <ZhangL2908@sduhsd.lan>
2016-09-06 11:42:52 -0700

    RENAMED

---
+++ APLesson_01/asciiart2.java
@@ -0,0 +1,10 @@
+public class asciiart2
+{
+	public static void main (String[]args)
+	{ 
+		System.out.println(" __    __");
+		System.out.println("/ \\\\..// \\");
+		System.out.println("  ( oo )  ");
+		System.out.println("   \\__/");
+	}
+}
--- APLesson_01/firstProg.java
+++ APLesson_01/firstProg.java
@@ -1,27 +1,7 @@
 public class firstProg
 {
-	public static void main (String[]args)
-	{ 
-	System.out.println("  / \\");
-	System.out.println(" / _ \\");
-	System.out.println("| / \\ |");
-	System.out.println("||   || _______");
-	System.out.println("||   || |\\     \\");
-	System.out.println("||   || ||\\     \\");
-	System.out.println("||   || || \\    |");
-	System.out.println(" ||   || ||  \\__/");
-	System.out.println(" ||   || ||   ||");
-	System.out.println("  \\\\_/ \\_/ \\\\_//");
-	System.out.println(" /   _     _   \\");
-	System.out.println("/               \\");
-	System.out.println("|    O     O    |");
-	System.out.println("|   \\  ___  /   |");
-	System.out.println("|    \\ \\_/ /    |");
-	System.out.println("/ ----- | ----- \\");
-	System.out.println("|   \\__/|\\__/   |");
-	System.out.println("\\     |_|_|     /");
-	System.out.println(" \\____     ____/");
-	System.out.println("      \\___/");
-	
+	public static void main(String[]args)
+	{
+		System.out.println("Hello World");
 	}
 }
