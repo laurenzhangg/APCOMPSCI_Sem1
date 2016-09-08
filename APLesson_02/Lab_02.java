93242f8cf13f27db3921e2e263e24a180e9ee12a
laurenzhangg <ZhangL2908@sduhsd.lan>
2016-09-08 11:07:20 -0700

    Finished Lab 2

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,26 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int x = 10;
+		int y = 3;
+		int mult = x * y;
+		
+		System.out.println("10 multiplied by 3 is " + mult);
+		
+		String first = "Lauren Zhang";
+		String second = "6368 Quail Run Street";
+		String third = "San Diego, CA 92130";
+		
+		System.out.println(first);
+		System.out.println(second);
+		System.out.println(third);
+		
+		int l = 5;
+		int w= 6;
+		int h = 7;
+		int SA= 5 * 6 * 7;
+		
+		System.out.println("The surface area of your rectangle is " + SA);
+	}
+}
