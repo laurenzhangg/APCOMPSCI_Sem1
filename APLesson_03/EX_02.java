132cbcf801f5333a1dc1080b6c42ddf813be98c1
laurenzhangg <ZhangL2908@sduhsd.lan>
2016-09-12 10:40:48 -0700

    Started lesson 2

---
+++ APLesson_03/EX_02.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class numbers
+{
+	Scanner keyboard = new Scanner(System.in);
+	System.out.println("What is your height in inches?");
+	float height = keyboard.next();
+	
+}
