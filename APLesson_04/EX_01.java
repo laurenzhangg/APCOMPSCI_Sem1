ad342815543d73bc8b657cb173eac7895c70bb0b
laurenzhangg <lauren.zhang2007@gmail.com>
2016-09-20 12:09:27 -0700

    Worked on Lesson 04

--- APLesson_04/EX_01.java
+++ APLesson_04/EX_01.java
@@ -8,28 +8,52 @@ public class EX_01
 		Scanner keyboard = new Scanner(System.in);
 		
 		System.out.println("Reciept Printer");
-		
+		//item1 inputs
 		System.out.println("Please enter item 1:");
-		String one = keyboard.next();
+		String one = keyboard.nextLine();
 		System.out.println("Please enter the price:");
-		double priceone	= keyboard.nextInt();
+		double priceone	= keyboard.nextDouble();
+		keyboard.nextLine();
 		
+		//item2 inputs
 		System.out.println("Please eneter item 2:");
-		String two = keyboard.next();
+		String two = keyboard.nextLine();
 		System.out.println("Please enter the price:");
-		double pricetwo = keyboard.nextInt();
+		double pricetwo = keyboard.nextDouble();
+		keyboard.nextLine();
 		
+		//item3 inputs
 		System.out.println("Please enter item 3:");
-		String three = keyboard.next();
+		String three = keyboard.nextLine();
 		System.out.println("Please enter the price:");
-		double pricethree = keyboard.nextInt();
-		
+		double pricethree = keyboard.nextDouble();
+			
 		System.out.println("<<<<<<<<<<<<<<<_Reciept_>>>>>>>>>>>>>>>");
 		
 		form.format(one, priceone);
 		form.format(two, pricetwo);
 		form.format(three, pricethree);
-		System.out.println("                 ");
-		System.out.println("                 ");
+		System.out.println("                      ");
+		System.out.println("                      ");
+		
+		double Subtotal = (priceone + pricetwo + pricethree);
+		double Tax = (Subtotal * .08);
+		double Total = (Subtotal + Tax);
+		
+		form.format("Subtotal:" , Subtotal);
+		form.format("Tax:" , Tax);
+		form.format("Total:" , Total);
+		
+		System.out.println("\n* Thank you for your support *");
 	}
+	public void format(String item, double price)
+	{
+		System.out.printf("\n%20s  %10.2f", item, price);
+	}
+	
+	
+	
+	
+	
+	
 }
