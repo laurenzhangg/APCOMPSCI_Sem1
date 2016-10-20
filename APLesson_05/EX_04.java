5a5e4492dc9e448f42382285722050398097843c
laurenzhangg <lauren.zhang2007@gmail.com>
2016-10-20 12:08:26 -0700

    finished 5.1 began 5.2

--- APLesson_05/EX_04.java
+++ APLesson_05/EX_04.java
@@ -1,16 +1,39 @@
 import java.util.Scanner;
 public class EX_04
 {
+	static double weight;
+	static double height;
+	static double BMI;
+	static String condition;
+	
 	public static void main(String[]args)
 	{
 		Scanner kb = new Scanner(System.in);
 		System.out.println("What is your height in inches?");
-		double height = kb.nextDouble();
+		height = kb.nextDouble();
 		System.out.println("What is your weight in pounds?") ;
-		double weight = kb.nextDouble();
+		weight = kb.nextDouble();
+	
+		calcBMI();
+	
+		System.out.println("Your BMI is: " + BMI + ".");
+		System.out.println("You are " + condition + ".");
+	}
+	public static void calcBMI()
+	{
+		BMI = (weight*703) / (height*height);
 		
-		double BMI = weight*703 / height*height;
-		System.out.println("Your BMI is: " + BMI);
-		
+		if(BMI < 18.5)
+			condition = "Underweight";
+		else if(BMI< 25)
+			condition = "Normal";
+		else if(BMI < 30)
+			condition = "Overweight";
+		else if(BMI < 35)
+			condition = "Obese";
+		else if(BMI < 40)
+			condition = "Very Obese";
+		else
+			condition = "Morbidly Obese";
 	}
 }
