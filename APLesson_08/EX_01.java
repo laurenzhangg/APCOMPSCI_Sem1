520489adde2770a3dffeaf9171edb5d60f1c9628
laurenzhangg <lauren.zhang2007@gmail.com>
2016-11-09 12:11:55 -0800

    Finished lesson

---
+++ APLesson_08/EX_01.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class EX_01
+{
+	static String sentence;
+	
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter a sentence: ");
+		sentence = kb.nextLine();
+		System.out.println(replace(sentence));
+	}
+	public static String replace(String sentence)
+	{
+		if(sentence.indexOf(" ") <= 0)
+			{
+				return sentence;
+			}
+		else
+		{
+			sentence = sentence.substring(0, sentence.indexOf(" ")) + "_" + sentence.substring(sentence.indexOf(" ") +1);
+			return replace(sentence);
+		}
+	}
+	
+	
+}
