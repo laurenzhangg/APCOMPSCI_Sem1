b14b8e1caedb288fa936569869131839a2fe9b10
laurenzhangg <lauren.zhang2007@gmail.com>
2017-02-24 12:13:26 -0800

    hi

---
+++ APLesson_13/XBox.java
@@ -0,0 +1,15 @@
+public class XBox extends Console
+{
+	public Xbox
+	{
+		super();
+	}
+	public String getController()
+	{
+		return "XBox Wireless Controller"
+	}
+	public String getPlatform()
+	{
+		return "XBox"
+	}
+}
